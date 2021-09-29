2019929b9882e9405e2f248a33e3554b326eabac
Alex Bucknall <alex.bucknall@gmail.com>
2021-09-29 22:38:42 +0100

    Added device tagging and parallel checking for performance.
    Change-type: minor
    Signed-off-by: Alex Bucknall <alex.bucknall@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 I2C script to check depop balenaFin profiles for customers.
 
-This specific example looks for the `pca963x` driver, attempts to set the blue led to 0 brightness and records if it succeeded or failed.
+This specific example looks for the `pca963x` driver, attempts to set the blue led to 0 brightness and records the device UUID if it succeeded or failed.
 
 ## Requirements
 
@@ -13,12 +13,14 @@ This specific example looks for the `pca963x` driver, attempts to set the blue l
 
 ## Usage
 
-Run the script with the following command:
+Run the script with the following command, where `$TAG` given with a value of "tag" is an optional arg to tag the devices in that fleet:
 
 ```bash
-./scan.sh $YOUR_BALENA_API_AUTH_TOKEN $FLEET_ID
+./scan.sh $YOUR_BALENA_API_AUTH_TOKEN $FLEET_ID $TAG
 ```
 
 > You can find you `$FLEET_ID` from the url, for example https://dashboard.balena-cloud.com/fleets/1234567, where 1234567 is the `$FLEET_ID`.
 
 The script will create a `suspect_devices.txt` file containing a list of the UUIDs of the devices with the missing I2C RGB LED controller.
+Set `$NUM_PROC` in the script for the number of simultaneous processes to run.
+This may help performance in large fleets.
--- scan.sh
+++ scan.sh
@@ -2,8 +2,10 @@
 
 BALENA_FLEET_AUTH_TOKEN=$1
 BALENA_FLEET_ID=$2
+TAG_DEVICE=$3
+NUM_PROC=4
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
   echo "Usage: ./scan.sh \$BALENA_FLEET_AUTH_TOKEN \$BALENA_FLEET_ID" >&2
   exit 1
 fi
@@ -19,36 +21,66 @@ for command in "balena" "jq" "curl"; do
 done
 
 # Get all device UUIDs in fleet
-> fleet.json && > suspect_devices.txt || true
-curl -X GET \
+> suspect_devices.txt || true
+fleet=$(curl -X GET \
 "https://api.balena-cloud.com/v6/application(${BALENA_FLEET_ID})?\$expand=owns__device" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ${BALENA_FLEET_AUTH_TOKEN}" \
--o fleet.json \
--s
+-s)
 
 # Write devices to text file
-jq -r '.d | .[] | .owns__device[] | "\(.uuid) \(.is_online) \(.api_heartbeat_state)"' fleet.json > fleet_devices.txt
+jq -r '.d | .[] | .owns__device[] | "\(.uuid) \(.id) \(.is_online) \(.api_heartbeat_state)"' <<< $fleet > fleet_devices.txt
 
 # Check if devices are online and have an api heartbeat
-online_devices=()
+online_devices_uuid=()
 while IFS= read -r line; do
   if [[ $line == *"true online"* ]]; then
     uuid=($line)
-    online_devices+=(${uuid[0]})
+    online_devices_uuid+=(${uuid[0]})
   fi
 done <fleet_devices.txt
 
 # Check if device has pca963x RGB LED controller
 > suspect_devices.txt || true
-for device in ${online_devices[@]}; do
-  echo UUID: $device, checking I2C...
-  result=$(echo 'echo 0 > /sys/class/leds/pca963x\:blue/brightness; dmesg | grep "pca963x"; exit;' | balena ssh $device | tail -n +4)
-  if [[ $result == *"failed"* ]]; then
-    echo $device has missing I2C RGB driver
-    echo $device >> suspect_devices.txt
+
+for uuid in ${online_devices_uuid[@]}; do
+  (
+    echo -e "UUID: $uuid, checking I2C for controller..."
+    result=$(echo 'echo 0 > /sys/class/leds/pca963x\:blue/brightness; dmesg | grep "pca963x"; exit;' | balena ssh $uuid | tail -n +4)
+    if [[ $result == *"failed"* ]]; then
+      echo -e "This device has missing I2C controller\n"
+      echo $uuid >> suspect_devices.txt
+    fi
+  ) &
+
+  if [[ $(jobs -r -p | wc -l) -ge $NUM_PROC ]]; then
+    # $NUM_PROC jobs are running, so wait here for jobs to finish.
+    wait -n
   fi
 done
 
+wait
+
+
+# Tag device
+if [ $3 = "tag" ]; then
+    online_device_ids=()
+    while IFS= read -r line; do
+        uuid=($line)
+        id=$(jq -r --arg UUID "$uuid" '.d | .[] | .owns__device[] | select(.uuid | contains($UUID)) | "\(.id)"' <<< $fleet)
+        echo -e "Tagging device with ID $id as a suspected device.\n"
+        tagged=$(curl -X POST \
+        "https://api.balena-cloud.com/v6/device_tag" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${BALENA_FLEET_AUTH_TOKEN}" \
+        --data '{
+            "device": "'${id}'",
+            "tag_key": "DEPOP",
+            "value": "pca963x"
+        }' \
+        -s)
+    done <suspect_devices.txt
+fi
+
 # Cleanup
-rm fleet.json fleet_devices.txt
+rm fleet_devices.txt
